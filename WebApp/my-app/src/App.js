1dddc464e08fa1975d790286fdde504e760690be
Box-Of-Hats <ja1ke@hotmail.co.uk>
2019-01-23 00:17:28 +0000

    Add foreground style change, with right click. Add lots of missing assets. Convert window preview sizings to be percentages throughout. Add example text to window preview.

--- WebApp/my-app/src/App.js
+++ WebApp/my-app/src/App.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
 import './style/App.css';
 const vsCodeLogo = require('./img/vsCodeLogo.png');
+const defaultSettings = require('./default.json');
+const icon0 = "💢";
+const icon1 = "💖";
+const icon2 = "👁‍🗨";
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            palette: {
-                "c0": "#141A18",
-                "c1": "#1D2725",
-                "c2": "#30423F",
-                "c3": "#FFD3F1",
-                "c4": "#EFEFEF",
-                "c5": "#cdeeca",
-            },
-            assets: {
-                "activityBar.background": "c2",
-                "activityBar.foreground": "c4",
-                "editor.background": "c0",
-                "sideBar.background": "c1",
-                "statusBar.background": "c2",
-                "titleBar.background": "c3",
-            },
+            palette: defaultSettings.palette,
+            assets: defaultSettings.assets,
             selectedColorName: "c0"
         };
         this.handleColorChange = this.handleColorChange.bind(this);
@@ -55,7 +45,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="header"><img src={vsCodeLogo}></img><span>VSCode Theme Designer</span></div>
+                <div className="header"><img src={vsCodeLogo} alt="Visual Studio Code Logo"></img><span>VSCode Theme Designer</span></div>
                 <div className="appSideBarWrapper">
                     <div className="appSideBar">
                         <div className="colorPickers">
@@ -122,12 +112,66 @@ const Footer = ({ props }) => {
 }
 
 function Asset(props) {
+    var foreColor;
+    //TODO: Fix null check. Somehow values are getting into code, such as "undefined": "#efefef",
+    if (props.assetFore !== null) {
+        foreColor = props.palette[props.assets[props.assetFore]];
+    } else {
+        foreColor = "#efefef";
+    }
     return (
         <div className={`asset ${props.className}`} onClick={() => props.handleClick(props.assetName)}
-            style={props.style ? props.style : { backgroundColor: props.palette[props.assets[props.assetName]] }}>
+            onContextMenu={(e) => { e.preventDefault(); console.log(props.assetName); props.handleClick(props.assetFore) }}
+            style={props.style ? props.style : {
+                backgroundColor: props.palette[props.assets[props.assetName]],
+                color: foreColor
+            }
+            }>
             {props.inner}
         </div >
     );
+}
+
+function CodeExample(props) {
+    var l1 = "function CodeExample(props) {";
+    var l2 = "const indent = \"30px\";"
+    var l3 = "return <div>"
+    var l4 = "<span style={{ paddingLeft: indent }}>{this.props.innerText}</span>"
+    var l5 = "<CodeImage src={this.props.image}/>"
+    var l6 = "</div>"
+    return <div style={{ userSelect: "none" }}>
+        <span style={{}}>{l1}</span><br />
+        <span style={{ paddingLeft: "30px" }}>{l2}</span><br />
+        <span style={{ paddingLeft: "30px" }}>{l3}</span><br />
+        <span style={{ paddingLeft: "60px" }}>{l4}</span><br />
+        <span style={{ paddingLeft: "60px" }}>{l5}</span><br />
+        <span style={{ paddingLeft: "30px" }}>{l6}</span><br />
+        <span style={{}}>}</span><br />
+    </div>
+}
+
+function FileStructure(props) {
+    var lines = ["◢ WebApp", "\t▶ .VSCode", "\t◢ src", `\t\t${icon1} App.js`, "\t◢ style", `\t\t${icon2}index.css`, `\t\t${icon2}app.css`]
+    var parent = []
+    lines.map((line) => {
+        if (line.includes("\t\t")) {
+            parent.push(<div style={{ paddingLeft: "20%" }}>{line}</div>);
+        } else if (line.includes("\t")) {
+            parent.push(<div style={{ paddingLeft: "10%" }}>{line}</div>);
+        } else {
+            parent.push(<div>{line}</div>);
+        }
+    });
+    return parent;
+}
+
+function OpenEditors(props) {
+    var lines = [`${icon1} App.js WebApp\\my-app\\src`, "Settings", `${icon2}index.css WebApp\\my-app\\style`]
+    var parent = []
+    lines.map((line) => {
+        parent.push(<div className="openEditorText"> {line}</div >);
+    });
+    return parent;
 }
 
 class WindowPreview extends Component {
@@ -144,13 +188,30 @@ class WindowPreview extends Component {
         const { assets, palette } = this.props;
         return (
             <div className="windowPreview">
-                <Asset className="titleBar" assetName="titleBar.background" palette={palette} assets={assets} handleClick={this.handleClick} inner="File Edit Selection View Go Debug Terminal Help" />
-                <Asset className="activityBar" assetName="activityBar.background" palette={palette} assets={assets} handleClick={this.handleClick} />
-                <Asset className="panel" assetName="panel.background" palette={palette} assets={assets} handleClick={this.handleClick} />
-                <Asset className="sideBar" assetName="sideBar.background" palette={palette} assets={assets} handleClick={this.handleClick} />
-                <Asset className="editor" assetName="editor.background" palette={palette} assets={assets} handleClick={this.handleClick} />
-                <Asset className="statusBar" assetName="statusBar.background" palette={palette} assets={assets} handleClick={this.handleClick} />
-  
+                <Asset className="titleBar" assetName="titleBar.activeBackground" assetFore="titleBar.activeForeground" palette={palette} assets={assets} handleClick={this.handleClick} inner="File Edit Selection View Go Debug Terminal Help" />
+                <Asset className="activityBar" assetName="activityBar.background" assetFore="activityBar.foreground" palette={palette} assets={assets} handleClick={this.handleClick} />
+                <div className="tabs">
+                    <Asset className="tab activeTab" assetName="tab.activeBackground" assetFore="tab.activeForeground" palette={palette} assets={assets} handleClick={this.handleClick} inner={`${icon1} ActiveTab.js`} />
+                    <Asset className="tab inactiveTab" assetName="tab.inactiveBackground" assetFore="tab.inactiveForeground" palette={palette} assets={assets} handleClick={this.handleClick} inner={`${icon0} bg1.json`} />
+                    <Asset className="tab inactiveTab" assetName="tab.inactiveBackground" assetFore="tab.inactiveForeground" palette={palette} assets={assets} handleClick={this.handleClick} inner={`${icon0} bg2.json`} />
+                    <Asset className="editorGroupHeader" assetName="editorGroupHeader.tabsBackground" palette={palette} assets={assets} handleClick={this.handleClick} />
+                </div>
+                <div className="sideBarContainer">
+                    <Asset className="sideBar" assetName="sideBar.background" assetFore="sideBar.foreground" palette={palette} assets={assets} handleClick={this.handleClick} inner="EXPLORER" />
+                    <Asset className="sideBarSectionHeader" assetName="sideBarSectionHeader.background" assetFore="sideBarSectionHeader.foreground" palette={palette} assets={assets} handleClick={this.handleClick} inner="◢ OPEN EDITORS" />
+                    <Asset className="sideBar" assetName="sideBar.background" assetFore="sideBar.foreground" palette={palette} assets={assets} handleClick={this.handleClick} inner={<OpenEditors />} />
+                    <Asset className="sideBarSectionHeader" assetName="sideBarSectionHeader.background" assetFore="sideBarSectionHeader.foreground" palette={palette} assets={assets} handleClick={this.handleClick} inner="◢ MYPROJECTNAME" />
+                    <Asset className="sideBar" assetName="sideBar.background" assetFore="sideBar.foreground" palette={palette} assets={assets} handleClick={this.handleClick} inner={<FileStructure />} />
+                    <Asset className="sideBarSectionHeader" assetName="sideBarSectionHeader.background" assetFore="sideBarSectionHeader.foreground" palette={palette} assets={assets} handleClick={this.handleClick} inner="▶ OUTLINE" />
+                </div>
+                <div className="rightContainer">
+                    <Asset className="miniMapSection" assetName="editor.background" palette={palette} assets={assets} handleClick={this.handleClick} />
+                    <Asset className="scrollBar" assetName="scrollbarSlider.background" palette={palette} assets={assets} handleClick={this.handleClick} />
+                </div>
+                <Asset className="panel" assetName="panel.background" assetFore="terminal.foreground" palette={palette} assets={assets} handleClick={this.handleClick} inner="This is the terminal" />
+                <Asset className="editor" assetName="editor.background" palette={palette} assets={assets} handleClick={this.handleClick} inner={<CodeExample />} />
+                <Asset className="statusBar" assetName="statusBar.background" assetFore="statusBar.foreground" palette={palette} assets={assets} handleClick={this.handleClick} inner="master 🗘 ⓧ ⚠ 🛈 javascript | 🙿 App.js" />
+
             </div >
         )
     }
