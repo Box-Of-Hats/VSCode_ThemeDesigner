a5ca7ef7f5e106878be71e56d2fd35d13832a98f
Box-Of-Hats <ja1ke@hotmail.co.uk>
2019-01-30 23:19:10 +0000

    Change font of colorpicker

--- WebApp/my-app/src/ColorPicker.jsx
+++ WebApp/my-app/src/ColorPicker.jsx
@@ -31,7 +31,7 @@ class ColorPicker extends Component {
                 <label className="colorPickerComponent__label" style={{ backgroundColor: this.props.color }}>
                     <input style={{ display: "none", overflow: "hidden", width: 0 }} onChange={this.handleColorChange} value={this.state.value} type="color"></input>
                 </label>
-                <input style={{ boxShadow: this.props.selected ? `inset 10px 0 5px ${this.props.color}` : "none", border: `2px solid ${this.props.color}`}} className={`colorPickerComponent__button ${highlightClass}`} type="button" value="Select" onClick={this.handleSelectButtonClick} />
+                <input style={{ boxShadow: this.props.selected ? `inset 10px 1px 0px ${this.props.color}` : "none", border: `2px solid ${this.props.color}`}} className={`colorPickerComponent__button ${highlightClass}`} type="button" value="Select" onClick={this.handleSelectButtonClick} />
             </div>
         );
     }
