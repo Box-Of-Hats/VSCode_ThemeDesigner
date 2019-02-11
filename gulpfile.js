5f6ff5e57cc27056f02e10ee0356accc4b583903
Box-Of-Hats <ja1ke@hotmail.co.uk>
2019-02-11 20:54:50 +0000

    Add minify css task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 let gulp = require("gulp");
 let less = require('gulp-less');
+let cleanCSS = require('gulp-clean-css');
 
-gulp.task("compile-less", function() {
+gulp.task("compile-less", () => {
     return gulp.src('./src/style/*.less')
         .pipe(less())
         .pipe(gulp.dest('./src/style/'))
 });
 
+gulp.task("minify-css", () => {
+    return gulp.src('./src/style/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./src/style'));
+});
 
-gulp.task("default", gulp.parallel('compile-less'));
+
+gulp.task("default", gulp.series(['compile-less', "minify-css"]));
