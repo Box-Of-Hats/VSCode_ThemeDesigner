88b56788b6706d9dc01106c2cb013f4d7c5e3e2c
Box-Of-Hats <ja1ke@hotmail.co.uk>
2020-09-27 00:14:45 +0100

    add download button

--- src/CodePreview.jsx
+++ src/CodePreview.jsx
@@ -1,38 +1,76 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CodePreview extends Component {
-    constructor(props) {
-        super(props)
-        this.generatePreviewText = this.generatePreviewText.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.generatePreviewText = this.generatePreviewText.bind(this);
+	}
 
-    generatePreviewText() {
-        var text = "\"workbench.colorCustomizations\": {\n";
-        Object.keys(this.props.assets).forEach(key => {
-            text = text + "\"" + key + "\": \"" + this.props.palette[this.props.assets[key]] + "\",\n";
-        });
-        text = text + "},";
-        return text
-    }
+	generatePreviewText() {
+		var text = '"workbench.colorCustomizations": {\n';
+		const allAssets = Object.keys(this.props.assets);
+		allAssets.forEach((key, index) => {
+			text =
+				text +
+				'"' +
+				key +
+				'": "' +
+				this.props.palette[this.props.assets[key]] +
+				(index === allAssets.length - 1 ? '"\n' : '",\n');
+		});
+		text = text + "}";
+		return text;
+	}
 
-    copyTextToClipboard(){
-        let textArea = document.getElementsByClassName("codePreview")[0];
-        textArea.select();
-        document.execCommand("copy");
-    }
+	copyTextToClipboard() {
+		let textArea = document.getElementsByClassName("codePreview")[0];
+		textArea.select();
+		document.execCommand("copy");
+	}
 
-    render() {
-        var codePreview = this.generatePreviewText();
+	download(filename, content) {
+		const downloadElement = document.createElement("a");
+		downloadElement.setAttribute(
+			"href",
+			"data:text/plain;charset=utf-8," + encodeURIComponent(content)
+		);
+		downloadElement.setAttribute("download", filename);
 
-        return (
-            <div className="codePreviewContainer">
-                <textarea className="codePreview" value={codePreview} readOnly>
-                </textarea >
-                <div className="button button--blue" onClick={this.copyTextToClipboard}><i class="material-icons">file_copy</i> Copy</div>
-                <div className="button button--minus"><i class="material-icons">save_alt</i> save</div>
-            </div>
-        )
-    }
+		downloadElement.style.display = "none";
+		document.body.appendChild(downloadElement);
+
+		downloadElement.click();
+
+		document.body.removeChild(downloadElement);
+	}
+
+	render() {
+		var codePreview = this.generatePreviewText();
+
+		return (
+			<div className="codePreviewContainer">
+				<textarea
+					className="codePreview"
+					value={codePreview}
+					readOnly
+				/>
+				<button
+					className="button button--blue"
+					onClick={this.copyTextToClipboard}
+				>
+					<i class="material-icons">file_copy</i> Copy
+				</button>
+				<button
+					className="button button--minus"
+					onClick={() => {
+						this.download("code-theme.json", `{${codePreview}}`);
+					}}
+				>
+					<i class="material-icons">save_alt</i> save
+				</button>
+			</div>
+		);
+	}
 }
 
 export default CodePreview;
